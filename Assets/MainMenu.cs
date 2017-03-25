4041f1d72e9358e4f1f7a4f1c0bca5e9e2aa622b
Alex <jurcaua@gmail.com>
2017-03-25 15:48:35 -0400

    main menu + ar-j animations

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void GoToMain(){
+		SceneManager.LoadScene ("hellyea");
+	}
+}
